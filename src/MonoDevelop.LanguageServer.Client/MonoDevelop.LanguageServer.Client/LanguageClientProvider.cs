b29afef3710d2ee117a52700ab6a968ba08e3565
Matt Ward <ward.matt@gmail.com>
2017-12-21 13:19:20 +0000

    Map language clients to file extensions
    Use MEF to create the language clients that implement the
    ILanguageClient interface.
    
    Use the IFileExtensionRegistryService to map a file extension to
    a content type. The content type can then be used to find a
    language client. The IFileExtensionRegistryService is provided by
    the Microsoft.VisualStudio.CoreUtility assembly and maps
    file name extensions to content type definitions for those
    defined using FileExtensionToContentTypeDefinition.
    
    Added a language server text editor extension which does not do
    anything yet apart from try to find a language client for a file
    extension.

---
+++ src/MonoDevelop.LanguageServer.Client/MonoDevelop.LanguageServer.Client/LanguageClientProvider.cs
@@ -0,0 +1,84 @@
+﻿//
+// LanguageClientProvider.cs
+//
+// Author:
+//       Matt Ward <matt.ward@xamarin.com>
+//
+// Copyright (c) 2017 Xamarin Inc. (http://xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+using MonoDevelop.Core;
+
+namespace MonoDevelop.LanguageServer.Client
+{
+	[Export]
+	class LanguageClientProvider
+	{
+		IFileExtensionRegistryService fileExtensionRegistry;
+		List<ILanguageClient> clients;
+
+		Dictionary<string, ILanguageClient> contentTypeMappings =
+			new Dictionary<string, ILanguageClient> (StringComparer.OrdinalIgnoreCase);
+
+		[ImportingConstructor]
+		public LanguageClientProvider (
+			[Import] IFileExtensionRegistryService fileExtensionRegistry,
+			[ImportMany (typeof (ILanguageClient))] IEnumerable<ILanguageClient> clients)
+		{
+			this.fileExtensionRegistry = fileExtensionRegistry;
+			this.clients = clients.ToList ();
+		}
+
+		public IEnumerable<ILanguageClient> Clients {
+			get { return clients; }
+		}
+
+		public void Initialize ()
+		{
+			GetContentTypeMappings ();
+		}
+
+		void GetContentTypeMappings ()
+		{
+			foreach (ILanguageClient client in clients) {
+				foreach (var contentType in client.GetType ().GetCustomAttributes<ContentTypeAttribute> ()) {
+					contentTypeMappings [contentType.ContentTypes] = client;
+				}
+			}
+		}
+
+		public bool HasLanguageClient (FilePath fileName)
+		{
+			IContentType contentType = fileExtensionRegistry.GetContentTypeForExtension (fileName.Extension);
+			if (contentType.IsUnknown ()) {
+				return false;
+			}
+
+			return contentTypeMappings.ContainsKey (contentType.TypeName);
+		}
+	}
+}
